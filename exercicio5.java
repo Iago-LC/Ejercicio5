bd1abb05426090d8e8afc1d616a1065fe81db532
Iago-LC <iagolc9@gmail.com>
2020-12-09 22:53:38 +0100

    Variables declaradas

--- exercicio5.java
+++ exercicio5.java
@@ -3,3 +3,14 @@ package boletín.pkg2;
 import java.util.Scanner;
 
 public class Exercicio11 {
+	public static void main(String[] args) {
+        int dia;
+        int mes;
+        int año;
+        int suma = 0;
+        int suma2 = 0;
+        int cifra;
+        int cifra2;
+        Scanner sc = new Scanner(System.in);
+
+
